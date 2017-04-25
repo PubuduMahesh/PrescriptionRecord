6e14fbe3ed9c1845adc0fd3daa35a783eef27801
Pubudu Meththananda <PMeththananda@cambio.se>
2017-04-25 09:13:19 +0530

    change the prefered size of the menu bar

--- cambio/precriptionrecord/view/MenuBar.java
+++ cambio/precriptionrecord/view/MenuBar.java
@@ -1,28 +1,40 @@
 package cambio.precriptionrecord.view;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.BorderFactory;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 
 public class MenuBar extends JPanel{
 	public MenuBar() {
-		JMenuBar menuBar = new JMenuBar();		
+		GridBagConstraints menuBarConstraints = new GridBagConstraints();
+		this.setLayout(new GridBagLayout());
+		JMenuBar menuBar = new JMenuBar();	
+		menuBar.setPreferredSize(new Dimension(650,30));
 		addMenuBarItem(menuBar);
-		this.add(menuBar);
+		setPreferredSize(new Dimension(700,35));
+		this.setBorder(BorderFactory.createLineBorder(Color.black));
+		this.add(menuBar,menuBarConstraints);
 	}	
 	
 	private void addMenuBarItem(JMenuBar menuBar){
-		JMenu home = new JMenu("Home");
-		JMenu patientManage = new JMenu("PatientManagement");
-		JMenu doctorManagement = new JMenu("DoctorManagement");
-		JMenu prescriptionManagement = new JMenu("PrescriptionManagement");
-		JMenu drugManagement = new JMenu("drugManagement");
+		JMenu home = new JMenu("Home  ");
+		JMenu patientManage = new JMenu("Patient Management  ");
+		JMenu doctorManagement = new JMenu("Doctor Management  ");
+		JMenu prescriptionManagement = new JMenu("Prescription Management  ");
+		JMenu drugManagement = new JMenu("Drug Management  ");
+		JMenu help = new JMenu("Help  ");
 		
 		menuBar.add(home);
 		menuBar.add(patientManage);
 		menuBar.add(doctorManagement);
 		menuBar.add(prescriptionManagement);
 		menuBar.add(drugManagement);
+		menuBar.add(help);
 	}
 }
